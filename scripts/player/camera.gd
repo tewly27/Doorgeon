49d559803af09eda1f8b46e42e15633603b7376b
GarniyHlopchik <Denisbuntovsky@gmail.com>
2024-02-12 15:40:24 +0300

    camera script added

---
+++ scripts/player/camera.gd
@@ -0,0 +1,31 @@
+extends Node2D
+@onready var player = get_parent()
+@export var moveTime: float
+@export var aheadDistance: float = 300
+@export var verticalMoveTime: float
+@export var verticalMoveSpeed: float
+
+@export var y_follow = true
+@export var x_follow = true
+var vertical_look_direction: float
+func _ready() -> void:
+	pass
+
+
+
+func _process(delta: float) -> void:
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	#x axis movement
+	if x_follow:
+		tween.tween_property(self,"global_position:x",player.global_position.x+player.direction*aheadDistance,moveTime)
+	
+	#y axis movement
+	vertical_look_direction = Input.get_axis("look_up","look_down")
+	if y_follow:
+		if vertical_look_direction:
+			tween.tween_property(self,"global_position:y",player.global_position.y+vertical_look_direction*aheadDistance,verticalMoveTime)
+		
+		else:
+			tween.tween_property(self,"global_position:y",player.global_position.y,verticalMoveTime)
+
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -4,11 +4,10 @@ extends CharacterBody2D
 @export var speed: float
 @export var jump_velocity: float
 @export var gravity: float
-
+var direction: float
 var inJump = false
 
 func _physics_process(delta: float) -> void:
-	print(velocity.y)
 	# Add the gravity.
 	if not is_on_floor():
 		#if the timer isn't running and you just began to fall, start coyote timer
@@ -17,7 +16,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 
 	# either if you are on ground or in coyote time, jump on button press
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or $CoyoteTimer.time_left != 0):
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or $CoyoteTimer.time_left != 0) and not inJump:
 		$JumpTimer.start()
 		velocity.y = jump_velocity
 		inJump = true
@@ -26,7 +25,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("jump") and inJump and $JumpTimer.time_left != 0:
 		velocity.y += jump_velocity
 
-	var direction := Input.get_axis("move_left", "move_right")
+	direction = Input.get_axis("move_left", "move_right")
 	var tween = get_tree().create_tween()
 	
 	tween.tween_property(self,"velocity:x",direction*speed,acceleration_time)
